e5b436927794bd1ef1a83f6d3bf37aeec935b85a
cbiteau <charly.biteau@with.fr>
2016-07-15 16:56:13 +0100

    Transform App on Class Based Component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,20 +2,31 @@ import React from 'react';
 import Notes from './Notes';
 import uuid from 'uuid';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'DO Laundry'
-  }
-]
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default() => (
-  <div>
-    <button onClick={() => console.log('add note')}>+</button>
-    <Notes notes={notes}/>
-  </div>
-);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+  render() {
+    const {notes} = this.state;
+
+    return (
+      <div>
+        <button onClick={() => console.log('add note')}>+</button>
+        <Notes notes={notes} />
+      </div>
+    )
+  }
+}
