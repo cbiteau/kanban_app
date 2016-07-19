b5c3ffb8f7fd45bbd22cb89de87cb403dc1d0130
cbiteau <charly.biteau@with.fr>
2016-07-19 16:59:14 +0100

    First Flux Implementation

--- app/libs/connect.jsx
+++ app/libs/connect.jsx
@@ -9,3 +9,55 @@ export default (state, actions) => {
     <target {...Object.assign({}, props, actions)} />
   );
 }
+
+// Connect to Alt through context. This hasn't been optimized
+// at all. If Alt store changes, it will force render.
+//
+// See *AltContainer* and *connect-alt* for optimized solutions.
+function connect(state = () => {}, actions = {}, target) {
+  class Connect extends React.Component {
+    componentDidMount() {
+      const {flux} = this.context;
+
+      flux.FinalStore.listen(this.handleChange);
+    }
+    componentWillUnmount() {
+      const {flux} = this.context;
+
+      flux.FinalStore.unlisten(this.handleChange);
+    }
+    render() {
+      const {flux} = this.context;
+      const stores = flux.stores;
+      const composedStores = composeStores(stores);
+
+      return React.createElement(target,
+        {...Object.assign(
+          {}, this.props, state(composedStores), actions
+        )}
+      );
+    }
+    handleChange = () => {
+      this.forceUpdate();
+    }
+  }
+  Connect.contextTypes = {
+    flux: React.PropTypes.object.isRequired
+  }
+
+  return Connect;
+}
+
+// Transform {store: <AltStore>} to {<store>: store.getState()}
+function composeStores(stores) {
+  let ret = {};
+
+  Object.keys(stores).forEach(k => {
+    const store = stores[k];
+
+    //Combine store state
+    ret = Object.assign({}, ret, store.getState());
+  });
+
+  return ret;
+}
